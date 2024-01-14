41e0955e197414ee5da9d88dc8384dd59c3ddd71
Mikhail Batakov <mihailbatakov@yandex.ru>
2024-01-14 13:20:28 +0500

    пр-13 автотесты, добавлены обязательные поля

--- models/user.js
+++ models/user.js
@@ -6,11 +6,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
+    required: true,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
+    required: true,
   },
   avatar: {
     type: String,
